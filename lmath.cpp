31e942c18931621286c6f31539dfc81650d64706
Daniil <daniil.kudinov@at.urfu.ru>
2023-03-15 14:55:52 +0500

    init

---
+++ lmath.cpp
@@ -0,0 +1,9 @@
+#include "lmath.hpp"
+
+namespace lmath{
+
+    int sum(int a, int b){
+        return a + b;
+    }
+
+}
---
+++ lmath.hpp
@@ -0,0 +1,10 @@
+#ifndef LMATH_H_
+#define LMATH_H_
+
+namespace lmath{
+
+    int sum(int a, int b);
+
+}
+
+#endif //LMATH_H_
